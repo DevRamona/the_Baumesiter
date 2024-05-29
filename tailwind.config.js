fb69a54d48f22858bd9d383a439ecef879cb14bc
DevRamona <r.ingabire@alustudent.com>
2024-05-29 17:32:15 +0200

    chores:remove the empty spaces

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ module.exports = {
   content: ["./*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     extend: {
+      lineHeight: {
+        'extra-loose': '3.5rem',
+      },
       colors:{
         primaryGreen: "#00672E",
         bgPrimary: "#EFECEA"
+      }, 
+      fontSize: {
+        'custom': '2.875rem',  // Add your custom font size class here
       }
     },
     fontFamily: {
