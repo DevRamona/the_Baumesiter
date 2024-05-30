2924c1b59dde9cace32e43a25876daec7a0a2ebe
DevRamona <r.ingabire@alustudent.com>
2024-05-30 16:49:40 +0200

    feat:create the reference section with text and image

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     extend: {
+      height: {
+        'sm': '150vh',
+        'md': '150vh',
+        'lg': '1480px'
+      },
+      width: {
+        'w-46': '46vh'
+
+      },
       colors:{
         primaryGreen: "#00672E",
         bgPrimary: "#EFECEA"
