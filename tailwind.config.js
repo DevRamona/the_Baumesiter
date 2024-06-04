7bf73ffea169c77336ee01f5bb8ffd10e8cada5d
DevRamona <r.ingabire@alustudent.com>
2024-06-04 12:22:04 +0200

    chores:modify the tailwind configuration and remove unnecessary classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,12 @@ module.exports = {
   content: ["./*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     extend: {
-      height: {
-        'sm-reference': '65rem',
-        'md-reference': '120vh',
-        'lg-reference': '81.25rem'
-      },
-      width: {
-        'w-46-reference': '46vh',
-        'w-reference':'25rem'
-
-      },
       colors:{
         primaryGreen: "#00672E",
         bgPrimary: "#EFECEA"
       }, 
       fontSize: {
-        'custom': '2.875rem',  // Add your custom font size class here
+        'custom': '2.875rem',
       }
     },
     fontFamily: {
