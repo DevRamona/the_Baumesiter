da259dcecab224bcb74e111ff2b9cd7ce61db62a
Kevin-gram <k.nyiringan@alustudent.com>
2024-06-04 12:42:45 +0200

    chore:deleting un used button

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,31 +3,19 @@ module.exports = {
   content: ["./*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     extend: {
-<<<<<<< HEAD
       fontSize: {
         navSize: "0.9375rem",
-        smallSize:"2rem",
-        smallerSize:"0.938"
+        smallSize: "2rem",
+        smallerSize: "0.938",
       },
-      colors:{
+      colors: {
         primaryGreen: "#00672E",
-        PrimaryColor: "#EFECEA"
-=======
-      lineHeight: {
-        'extra-loose': '3.5rem',
+        PrimaryColor: "#EFECEA",
       },
-      colors:{
-        primaryGreen: "#00672E",
-        bgPrimary: "#EFECEA"
-      }, 
-      fontSize: {
-        'custom': '2.875rem',  // Add your custom font size class here
->>>>>>> 9d41d745a523c0a8eb09b4281e4e3026d3f2617a
-      }
     },
     fontFamily: {
-      body: ["Outfit"]
-    }
+      body: ["Outfit"],
+    },
   },
   plugins: [],
-}
+};
